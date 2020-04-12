942c514937c572a41243907720003b5d871a9fbf
Jason Kirsons <jkirsons@gmail.com>
2020-04-12 15:08:27 +0200

    rotate

--- Scripts/Camera.gd
+++ Scripts/Camera.gd
@@ -1,15 +1,22 @@
 extends Camera
 
-export var speed = 3.0
+export var speed = 6.0
+export var fixedMode := true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_as_toplevel(true)
 
 func _physics_process(delta):
-	var target = get_parent().get_global_transform().origin
 	var player = get_parent().get_parent().get_global_transform().origin
 	var pos = get_global_transform().origin
+	
+	var target 
+	if fixedMode:
+		target = player + get_parent().transform.origin
+	else:
+		target = get_parent().global_transform.origin
+		
 	var offset = pos - target
 	pos = pos - offset * delta * speed
 	look_at_from_position(pos, player, Vector3.UP)
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -2,6 +2,7 @@ extends KinematicBody
 
 export var gravity = -10
 export var max_speed = 4
+export var fixedMode := true
 
 onready var camera = get_node("/root/Spatial/Player/Target/Camera")
 var velocity = Vector3()
@@ -12,24 +13,31 @@ func _ready():
 
 func _physics_process(delta):
 	velocity.y += gravity * delta
-	var desired_velocity = get_input() * max_speed
+	var input = get_input()
+	var desired_velocity 
+	if fixedMode:
+		desired_velocity = input * max_speed
+	else:
+		desired_velocity = global_transform.basis * input.z
 
 	velocity.x = desired_velocity.x
 	velocity.z = desired_velocity.z
 	velocity = move_and_slide(velocity, Vector3.UP, true)
-	
+
+	if(input != Vector3.ZERO):
+		transform.basis = Basis(input, Vector3.UP, input.cross(Vector3.UP))
 
 func get_input():
 	var input_dir = Vector3()
-	var trans = camera.get_global_transform()
+	# var trans = camera.get_global_transform()
 	# desired move in camera direction
 	if Input.is_action_pressed("ui_up"):
-		input_dir += -trans.basis.z
+		input_dir += Vector3.FORWARD
 	if Input.is_action_pressed("ui_down"):
-		input_dir += trans.basis.z
+		input_dir += Vector3.BACK
 	if Input.is_action_pressed("ui_left"):
-		input_dir += -trans.basis.x
+		input_dir += Vector3.LEFT
 	if Input.is_action_pressed("ui_right"):
-		input_dir += trans.basis.x
+		input_dir += Vector3.RIGHT
 	input_dir = input_dir.normalized()
 	return input_dir
